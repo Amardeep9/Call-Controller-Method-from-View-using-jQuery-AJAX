5aba00ccfe358635d58bc0db1f753e8703888373
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-06-22 15:57:54 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CallingControusiJquer.Models;
+
+namespace CallingControusiJquer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+            PersonModel person = new PersonModel
+            {
+                Name = name,
+                DateTime = DateTime.Now.ToString()
+            };
+            return Json(person);
+        }
+    }
+}
---
+++ PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallingControusiJquer.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+
+      
+        public string DateTime { get; set; }
+    }
+}
